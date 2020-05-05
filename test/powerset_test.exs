e5edab8cb0da149cc677832ff1904aa698f6356b
Leo Nistor <leo.nistor@gmail.com>
2020-05-05 09:03:49 +0300

    simple tests

--- test/powerset_test.exs
+++ test/powerset_test.exs
@@ -1,8 +1,21 @@
 defmodule PowersetTest do
   use ExUnit.Case
-  doctest Powerset
 
-  test "greets the world" do
-    assert Powerset.hello() == :world
+  test "powerset_bits" do
+    expected = [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+    actual = Powerset.powerset_bits([1, 2, 3])
+    assert expected == actual
+  end
+
+  test "powerset_append" do
+    expected = [[1, 2, 3], [1, 2], [1, 3], [1], [2, 3], [2], [3], []]
+    actual = Powerset.powerset_append([1, 2, 3])
+    assert expected == actual
+  end
+
+  test "powerset_prepend" do
+    expected = [[1], [1, 3], [1, 2, 3], [1, 2], [2], [2, 3], [3], []]
+    actual = Powerset.powerset_prepend([1, 2, 3])
+    assert expected == actual
   end
 end
