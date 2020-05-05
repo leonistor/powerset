3da24c62ae39a5f34edfc15149e4d5f6efda6e28
Leo Nistor <leo.nistor@gmail.com>
2020-05-05 08:51:28 +0300

    implementation defs

--- lib/powerset.ex
+++ lib/powerset.ex
@@ -1,18 +1,35 @@
 defmodule Powerset do
+  use Bitwise
+
   @moduledoc """
-  Documentation for `Powerset`.
+  Powerset function implementations.
   """
 
-  @doc """
-  Hello world.
+  def powerset_bits(list) do
+    n = length(list)
+    max = round(:math.pow(2, n))
 
-  ## Examples
-
-      iex> Powerset.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    for i <- 0..(max - 1) do
+      for pos <- 0..(n - 1), band(i, bsl(1, pos)) != 0 do
+        Enum.at(list, pos)
+      end
+    end
   end
+
+  def powerset_append([]), do: [[]]
+
+  def powerset_append([h | t]) do
+    pt = powerset_append(t)
+    for(x <- pt, do: [h | x]) ++ pt
+  end
+
+  def powerset_prepend([]), do: [[]]
+
+  def powerset_prepend([h | t]) do
+    pt = powerset_prepend(t)
+    powerset_prepend(h, pt, pt)
+  end
+
+  defp powerset_prepend(_, [], acc), do: acc
+  defp powerset_prepend(x, [h | t], acc), do: powerset_prepend(x, t, [[x | h] | acc])
 end
